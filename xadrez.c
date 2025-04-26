680f2cef430506a2eee8d1f3e7b7496033f634a4
wesleyvn13 <202501691871@alunos.estacio.br>
2025-04-26 13:27:33 -0300

    adição da movimentação e da peca Cavalo do xadrez
    adição da peça e da movimentação do cavalo para baixo e para esquerda.

--- xadrez.c
+++ xadrez.c
@@ -6,7 +6,7 @@ int main() {
 
     // Declaração de variaveis para contador.
 
-    int cont1 = 1 , cont2 = 1;
+    int cont1 = 1, cont2 = 1, j = 1;
 
     // movimentação do torre. 
 
@@ -15,7 +15,7 @@ int main() {
     // O while vai repetir o bloco de codigo  5 vezes.
     while (cont1<=5) {
     
-        printf("Casa %d : Direita\n", cont1 );
+        printf("Movimento %d : Direita\n", cont1 );  // imprime 5 vezes "direita"
         
          cont1++;
     };
@@ -26,7 +26,7 @@ int main() {
 
     // O do-while vai repetir o bloco de codigo  5 vezes.
     do {
-        printf("Casa %d : Cima, Direita\n", cont2);
+        printf("Movimento %d : Cima, Direita\n", cont2);  // imprime 5 vezes "cima, direita"
         cont2++;
     } while (cont2<=5);
 
@@ -36,8 +36,25 @@ int main() {
 
     // O For vai repetir o bloco de codigo 8 vezes.
     for (int cont3 = 1 ; cont3 <=8 ; cont3++ ) {
-        printf("Casa %d : Esquerda\n", cont3);
+        printf("Movimento %d : Esquerda\n", cont3);  // imprime 8 vezes "esquerda"
     };
+
+
+    // movimentação do cavalo.
+
+    printf("\n $$$ CAVALO $$$");
+
+     // loop aninhado usando for e while.
+     // simula o movimento em "L" do cavalo para esquerda (nesse caso).
+     for(int cont4 = 1; cont4 == 1; cont4++ ) {
+        printf("\n");
+        while (j <=2) {
+            printf("Movimento %d : Baixo\n", j); // imprime 2 vezes "baixo"
+            j++;
+        }
+        printf("Movimento %d : Esquerda\n", cont4); // imprime 1 vez "esquerda"
+     }
+
 
     return 0;
 }
